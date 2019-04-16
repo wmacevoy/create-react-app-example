f757ba5a1bd7283c000754609d5987e37e755543
Warren MacEvoy <wmacevoy@gmail.com>
2019-04-16 14:59:00 -0600

    edit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+// Example usage: <ShoppingList name="Mark" />
+
 class App extends Component {
   render() {
     return (
